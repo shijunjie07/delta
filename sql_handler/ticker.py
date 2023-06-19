3946f3ac1fabff691193c2e81676327afd824dba
junja <shijunja@yahoo.com>
2023-06-19 10:43:38 +0800

    fix import error

--- sql_handler/ticker.py
+++ sql_handler/ticker.py
@@ -8,6 +8,137 @@ import pandas as pd
 import sqlite3
 import logging
 
+class GetData():
+    
+    def __init__(self, logger:logging.Logger, DB_PATH:str):
+        self.logger = logger
+        self.DB_PATH = DB_PATH
+        self.con = sqlite3.connect(self.DB_PATH, check_same_thread=False)
+        self.cur = self.con.cursor()
+
+
+    def pull_tkl_dts(
+        self, ticker:str, start_date:str,
+        end_date:str, start_ts:int, end_ts:int
+    ) -> tuple[list[str], list[int]]:
+        """pull existing dates and timestamps from database
+`
+        Args:
+            ticker (str): _description_
+            start_date (str): _description_
+            end_date (str): _description_
+            start_ts (int): _description_
+            end_ts (int): _description_
+
+        Returns:
+            tuple[list[str], list[int]]: _description_
+        """
+    
+        dt_obj_index = 0
+        
+        # queries
+        eod_query = 'SELECT date_day FROM {}_eod WHERE date_day>={} AND date_day<={};'.format(
+            ticker, start_date.replace('-', ''), end_date.replace('-', '')
+        )
+        intra_query = 'SELECT date_time FROM {}_intra WHERE date_time>={} AND date_time<={};'.format(
+            ticker, start_ts, end_ts
+        )
+        
+        # pull from database
+        raw_eod = self.con.execute(eod_query).fetchall()    # eod date "%Y%m%d"
+        raw_intra = self.con.execute(intra_query).fetchall() # intra datetime unix timestamp
+        
+        # extract dates
+        if (raw_eod):
+            dates = [str(x[dt_obj_index]) for x in raw_eod]
+            dates = [
+                '{}-{}-{}'.format(x[:4], x[:6], x[6:])
+                for x in dates
+            ]
+        else:
+            dates = []
+        
+        # extract timestamps
+        if (raw_intra):
+            timestamps = [int(x[dt_obj_index]) for x in raw_intra]
+        else:
+            timestamps = []
+
+        self.logger.info('existing dts: {}(date) {}(tss)'.format(len(dates), len(timestamps)))
+
+        return dates, timestamps
+    
+    def pull_eod():
+        ...
+    
+    def pull_intra():
+        ...
+
+
+class LoadData():
+    
+    def __init__(self, logger:logging.Logger, DB_PATH:str):
+        self.logger = logger
+        self.DB_PATH = DB_PATH
+        self.con = sqlite3.connect(self.DB_PATH, check_same_thread=False)
+        self.cur = self.con.cursor()
+
+    def push_intra(self, ticker:str, df:pd.DataFrame) -> bool:
+        """
+        Pushes the data from a pandas DataFrame into the corresponding intra-day table in the database.
+
+        Args:
+            ticker (str): The ticker symbol of the stock or financial instrument.
+            df (pd.DataFrame): The DataFrame containing the data to be pushed.
+
+        Returns:
+            bool: True if the data is successfully pushed, False otherwise.
+        """
+        self.logger.info('prepare for intra push')
+
+        # format column name
+        is_success_format, df = self._format_column_names(df, 'intra')
+        if (not is_success_format):
+            self.logger.info('* fail to push intra')
+            return False
+        
+        # push data
+        try:
+            df.to_sql('{}_intra'.format(ticker), self.con, if_exists='append', index=False)
+            self.con.commit()
+            self.logger.info('success push intra')
+            return True
+        except Exception as e:
+            self.logger.info('error occurred while pushing intra, {}'.format(ticker, e))
+            return False
+        
+    def push_eod(self, ticker: str, df: pd.DataFrame) -> bool:
+        """
+        Pushes the data from a pandas DataFrame into the corresponding end-of-day (EOD) table in the database.
+
+        Args:
+            ticker (str): The ticker symbol of the stock or financial instrument.
+            df (pd.DataFrame): The DataFrame containing the data to be pushed.
+
+        Returns:
+            bool: True if the data is successfully pushed, False otherwise.
+        """
+        self.logger.info('prepare for eod push')
+        # format column name
+        is_success_format, df = self._format_column_names(df, 'eod')
+        if (not is_success_format):
+            self.logger.info('* fail to push eod')
+            return False
+        
+        # push data
+        try:
+            df.to_sql('{}_eod'.format(ticker), self.con, if_exists='append', index=False)
+            self.con.commit()
+            self.logger.info('success push eod')
+            return True
+        except Exception as e:
+            self.logger.info('error occurred while pushing eod, {}'.format(ticker, e))
+            return False
 
 
 class TickerDB(GetData, LoadData):
@@ -156,135 +287,3 @@ class TickerDB(GetData, LoadData):
         # Return the DataFrame with the formatted column names
         return True, formatted_df 
 
-
-class GetData(tickerDB):
-    
-    def __init__(self, logger:logging.Logger, DB_PATH:str):
-        self.logger = logger
-        self.DB_PATH = DB_PATH
-        self.con = sqlite3.connect(self.DB_PATH, check_same_thread=False)
-        self.cur = self.con.cursor()
-
-
-    def pull_tkl_dts(
-        self, ticker:str, start_date:str,
-        end_date:str, start_ts:int, end_ts:int
-    ) -> tuple[list[str], list[int]]:
-        """pull existing dates and timestamps from database
-`
-        Args:
-            ticker (str): _description_
-            start_date (str): _description_
-            end_date (str): _description_
-            start_ts (int): _description_
-            end_ts (int): _description_
-
-        Returns:
-            tuple[list[str], list[int]]: _description_
-        """
-    
-        dt_obj_index = 0
-        
-        # queries
-        eod_query = 'SELECT date_day FROM {}_eod WHERE date_day>={} AND date_day<={};'.format(
-            ticker, start_date.replace('-', ''), end_date.replace('-', '')
-        )
-        intra_query = 'SELECT date_time FROM {}_intra WHERE date_time>={} AND date_time<={};'.format(
-            ticker, start_ts, end_ts
-        )
-        
-        # pull from database
-        raw_eod = self.con.execute(eod_query).fetchall()    # eod date "%Y%m%d"
-        raw_intra = self.con.execute(intra_query).fetchall() # intra datetime unix timestamp
-        
-        # extract dates
-        if (raw_eod):
-            dates = [str(x[dt_obj_index]) for x in raw_eod]
-            dates = [
-                '{}-{}-{}'.format(x[:4], x[:6], x[6:])
-                for x in dates
-            ]
-        else:
-            dates = []
-        
-        # extract timestamps
-        if (raw_intra):
-            timestamps = [int(x[dt_obj_index]) for x in raw_intra]
-        else:
-            timestamps = []
-
-        self.logger.info('existing dts: {}(date) {}(tss)'.format(len(dates), len(timestamps)))
-
-        return dates, timestamps
-    
-    def pull_eod():
-        ...
-    
-    def pull_intra():
-        ...
-
-
-class LoadData(tickerDB):
-    
-    def __init__(self, logger:logging.Logger, DB_PATH:str):
-        self.logger = logger
-        self.DB_PATH = DB_PATH
-        self.con = sqlite3.connect(self.DB_PATH, check_same_thread=False)
-        self.cur = self.con.cursor()
-
-    def push_intra(self, ticker:str, df:pd.DataFrame) -> bool:
-        """
-        Pushes the data from a pandas DataFrame into the corresponding intra-day table in the database.
-
-        Args:
-            ticker (str): The ticker symbol of the stock or financial instrument.
-            df (pd.DataFrame): The DataFrame containing the data to be pushed.
-
-        Returns:
-            bool: True if the data is successfully pushed, False otherwise.
-        """
-        self.logger.info('prepare for intra push')
-
-        # format column name
-        is_success_format, df = self._format_column_names(df, 'intra')
-        if (not is_success_format):
-            self.logger.info('* fail to push intra')
-            return False
-        
-        # push data
-        try:
-            df.to_sql('{}_intra'.format(ticker), self.con, if_exists='append', index=False)
-            self.con.commit()
-            self.logger.info('success push intra')
-            return True
-        except Exception as e:
-            self.logger.info('error occurred while pushing intra, {}'.format(ticker, e))
-            return False
-        
-    def push_eod(self, ticker: str, df: pd.DataFrame) -> bool:
-        """
-        Pushes the data from a pandas DataFrame into the corresponding end-of-day (EOD) table in the database.
-
-        Args:
-            ticker (str): The ticker symbol of the stock or financial instrument.
-            df (pd.DataFrame): The DataFrame containing the data to be pushed.
-
-        Returns:
-            bool: True if the data is successfully pushed, False otherwise.
-        """
-        self.logger.info('prepare for eod push')
-        # format column name
-        is_success_format, df = self._format_column_names(df, 'eod')
-        if (not is_success_format):
-            self.logger.info('* fail to push eod')
-            return False
-        
-        # push data
-        try:
-            df.to_sql('{}_eod'.format(ticker), self.con, if_exists='append', index=False)
-            self.con.commit()
-            self.logger.info('success push eod')
-            return True
-        except Exception as e:
-            self.logger.info('error occurred while pushing eod, {}'.format(ticker, e))
-            return False
